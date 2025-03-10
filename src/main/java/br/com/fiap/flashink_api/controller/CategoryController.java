400ef7967faf0379b58c7f7e88c752cb607d3201
levmn <levimncontato@gmail.com>
2025-03-10 20:53:36 -0300

    adding CRUD operations in the controller

--- src/main/java/br/com/fiap/flashink_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/flashink_api/controller/CategoryController.java
@@ -1,21 +1,73 @@
 package br.com.fiap.flashink_api.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.flashink_api.model.Category;
 
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
-    // Listar todas as categorias
-    // GET :8080/categories
-    @GetMapping("/categories")
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+    private List<Category> repository = new ArrayList<>();
+
+    @GetMapping
     public List<Category> index() {
-        return List.of(
-                new Category(1L, "Educação", "book"),
-                new Category(2L, "Lazer", "casino"),
-                new Category(3L, "Salário", "payments"));
+        return repository;
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> create(@RequestBody Category category) {
+        repository.add(category);
+        log.info("Cadastrando categoria " + category.getName());
+        return ResponseEntity.status(201).body(category);
+    }
+
+    @GetMapping("{id}")
+    public Category get(@PathVariable Long id) {
+        log.info("Buscando categoria " + id);
+        return getCategory(id);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
+        log.info("Apagando categoria " + id);
+        repository.remove(getCategory(id));
+    }
+
+    @PutMapping("{id}")
+    public Category update(@PathVariable Long id, @RequestBody Category category) {
+        log.info("Atualizando categoria " + category.toString());
+
+        repository.remove(getCategory(id));
+        category.setId(id);
+        repository.add(category);
+
+        return category;
+    }
+
+    private Category getCategory(Long id) {
+        return repository.stream()
+                .filter(c -> c.getId().equals(id))
+                .findFirst()
+                .orElseThrow(
+                        () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada, id=" + id));
     }
 }
--- src/main/java/br/com/fiap/flashink_api/model/Category.java
+++ src/main/java/br/com/fiap/flashink_api/model/Category.java
@@ -1,12 +1,14 @@
 package br.com.fiap.flashink_api.model;
 
+import java.util.Random;
+
 public class Category {
     private Long id;
     private String name;
     private String icon;
 
     public Category(Long id, String name, String icon) {
-        this.id = id;
+        this.id = Math.abs(new Random().nextLong());
         this.name = name;
         this.icon = icon;
     }
@@ -15,24 +17,21 @@ public class Category {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Long setId(Long id) {
+        return this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getIcon() {
         return icon;
     }
 
-    public void setIcon(String icon) {
-        this.icon = icon;
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + ", icon=" + icon + "]";
     }
 
 }
