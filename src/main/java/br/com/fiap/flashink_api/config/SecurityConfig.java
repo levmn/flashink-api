0ca7298396178826e021e82e0b9dfdf0f50bb893
allanbm100 <allan.bm.100@gmail.com>
2025-04-28 21:46:06 -0300

    SecurityConfig

---
+++ src/main/java/br/com/fiap/flashink_api/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package br.com.fiap.flashink_api.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/transactions/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                )
+            .httpBasic(Customizer.withDefaults())
+            .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(List.of(
+            User
+            .withUsername("allan")
+            .password("$2a$12$UOW2MMwt/cXWSUcRkvJguOeRUxxWX4fhZs.Cy6Minv9qI1LkXh2Pe")
+            .roles("ADMIN", "USER")
+            .build(),
+            User
+            .withUsername("levi")
+            .password("$2a$12$kiudxYUEistkSyYTWaOEEedey//oJ.VqOr1UO9ysz22mbNlYJYT5q")
+            .roles("ADMIN", "USER")
+            .build()
+        ));
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
